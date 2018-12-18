b054a3745c2704d37952a344eee5cf7cffa8b53b
Trejkaz (pen name) <trejkaz@trypticon.org>
2018-12-18 09:21:14 +0900

    Initial commit

---
+++ src/main/java/acme/Something.java
@@ -0,0 +1,7 @@
+package acme;
+
+public class Something {
+    public void doIt() {
+        System.out.println("Did it");
+    }
+}
---
+++ src/test/java/acme/TestSomething.java
@@ -0,0 +1,10 @@
+package acme;
+
+import org.junit.Test;
+
+public class TestSomething {
+    @Test
+    public void testDoIt() {
+        new Something().doIt();
+    }
+}
